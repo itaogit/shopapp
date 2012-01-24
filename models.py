eae0706cc832170fd9a7d70f155bc95659996fc5
rakehurst <rakehurst@i-tao.com>
2012-01-24 17:20:57 +0000

    image upload working

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Product(db.Model):
     name            = 'Test Product'
     description     = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet enim ac augue auctor viverra. Phasellus congue tempor justo sed cursus. Quisque non quam turpis. Curabitur mollis luctus tempor. Aliquam sit amet nisl vel arcu rutrum ornare at vel sem.'
     price           = '%.2f' % 10.00
-    images          = []
+    images          = ['1-1','1-2','1-3','1-4']
     tags            = ['keyword1','keyword2','Keyword3', 'keyword4']
     quantity        =  3
     options         = [{'Size':['Large','Medium','Small']},{'Colour':['Red','White','Blue']}]
